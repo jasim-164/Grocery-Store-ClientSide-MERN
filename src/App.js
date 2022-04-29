fddd7a59f764e156b88bd75ade6dba12fc2c5fbd
jasim-164 <jasimuddincse1703164@gmail.com>
2022-04-30 01:06:18 +0600

    nav-bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './componennts/Home/Home'
+import Header from './componennts/Header/Header'
+import Footer from './componennts/Footer/Footer'
 function App() {
+
   return (
     <div className="App">
     <h1>welcome to Grocery Stock</h1>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/header" element={<Header/>}/>
+    </Routes>
+    <Footer/>
     </div>
   );
 }
---
+++ src/componennts/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../images/Logo 3.png'
+const Header = () => {
+    return (
+        <div>
+        <div className="d-flex  mr-auto justify-content-around align-items-center">
+
+        <div >
+        <Link to='/home'><img src={logo} width='200' height='100'alt="logo" ></img></Link>
+        </div>
+        <div className="d-flex mr-auto">    
+        <Link to='/' className=" mr-auto p-4">Home</Link>
+        <Link to='/grocery' className="  p-4">Grocery-Items</Link>
+        <Link to='/blog' className=" p-4">Blog</Link>
+        <Link to='/login' className="  p-4">Login</Link></div>
+        </div>
+        <h1>header</h1>
+        </div>
+    );
+};
+
+export default Header;
